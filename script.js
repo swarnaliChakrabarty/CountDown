ae6be6d53427c78e3c0ed0c3fda1709db0b5ffd6
swarnaliChakrabarty <64632934+swarnaliChakrabarty@users.noreply.github.com>
2021-05-22 12:04:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const daysEl=document.getElementById('days')
+const hoursEl=document.getElementById('hours')
+const minsEl=document.getElementById('mins')
+const secondsEl=document.getElementById('seconds')
+
+const newYears="1 Jan 2021";
+
+function countdown(){
+    const newyearsDate=new Date(newYears);
+    const currentDate=new Date();
+    const totalseconds=(newyearsDate - currentDate)/1000;
+    const days=Math.floor(totalseconds / 3600 / 24);
+    const hours=Math.floor(totalseconds/3600)%24;
+    const mins=Math.floor(totalseconds/60)% 60;
+    const seconds=Math.floor(totalseconds)%60;
+    // console.log(days,hours,mins,seconds);
+
+    daysEl.innerHTML=days;
+
+    hoursEl.innerHTML=formatTime(hours);
+
+    minsEl.innerHTML=formatTime(mins);
+
+    secondsEl.innerHTML=formatTime(seconds);
+
+
+}
+function formatTime(time){
+    return time<10 ? (`0${time}`) : time;
+}
+countdown();
+setInterval(countdown,1000);
